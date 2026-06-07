f5696ff827ee6d2fe282bfd3a035900d05c5407d
Soroush <soroushposhtdar@gmail.com>
2026-06-07 10:56:11 +0330

    Updated and changed the file

--- hello.c
+++ hello.c
@@ -1,13 +1,7 @@
 #include <stdio.h>
-void red(){printf("\033[0;31m");}
-void Purple(){printf("\033[0;35m");}
-void Blue (){printf("\033[0;34m");}
-int main () {
-	red();
-	printf("Hello ");
-	Purple();
-	printf("world");
-	Blue();
-	printf("!!!\n");
+
+int main() {
+	printf("hello Github!!!!");
+
 	return 0;
 }

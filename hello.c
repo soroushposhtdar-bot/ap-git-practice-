71c5c5ce0b2e9abfdebd75ef4b8725c55941df06
Soroush <soroushposhtdar@gmail.com>
2026-06-07 10:54:29 +0330

    Updated and changed the file

--- hello.c
+++ hello.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
-
-int main() 
-{
-	printf("hello world");
-
+void red(){printf("\033[0;31m");}
+void Purple(){printf("\033[0;35m");}
+void Blue (){printf("\033[0;34m");}
+int main () {
+	red();
+	printf("Hello ");
+	Purple();
+	printf("world");
+	Blue();
+	printf("!!!\n");
 	return 0;
 }
